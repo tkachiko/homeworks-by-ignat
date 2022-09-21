96cf597e714765f06ab3fe7bbb6ed6f4b0105fdb
tkachiko <tkachikos@gmail.com>
2022-09-22 00:21:02 +0300

    homework 02 - add logic and types

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,38 +1,48 @@
-import React from 'react'
-import Affair from './Affair'
-import {AffairType} from './HW2'
+import React from 'react';
+import Affair from './Affair';
+import {AffairType, FilterType} from './HW2';
+import s from './Affairs.module.css';
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+  data: Array<AffairType>
+  setFilter: (value: FilterType) => void
+  deleteAffairCallback: (id: number) => void
 }
 
 function Affairs(props: AffairsPropsType) {
-    const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
-    ))
+  const mappedAffairs = props.data.map((a: AffairType) => (
+    <Affair // should work
+      key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+      affair={a}
+      deleteAffairCallback={props.deleteAffairCallback}
+    />
+  ));
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+  const setAll = () => {
+    return props.setFilter('all');
+  }; // need to fix
+  const setHigh = () => {
+    return props.setFilter('high');
+  };
+  const setMiddle = () => {
+    return props.setFilter('middle');
+  };
+  const setLow = () => {
+    return props.setFilter('low');
+  };
 
-    return (
-        <div>
+  return (
+    <div>
 
-            {mappedAffairs}
-
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
-        </div>
-    )
+      {mappedAffairs}
+      <div className={s.buttons}>
+        <button className={s.filterButton} onClick={setAll}>All</button>
+        <button className={s.filterButton} onClick={setHigh}>High</button>
+        <button className={s.filterButton} onClick={setMiddle}>Middle</button>
+        <button className={s.filterButton} onClick={setLow}>Low</button>
+      </div>
+    </div>
+  );
 }
 
-export default Affairs
+export default Affairs;
