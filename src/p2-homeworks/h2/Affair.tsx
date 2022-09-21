55ab74cd16fd5e2d8fd60272c767fbb56418f19a
tkachiko <tkachikos@gmail.com>
2022-09-22 00:21:05 +0300

    homework 02 - add logic and types

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {AffairType} from './HW2';
+import s from './Affairs.module.css';
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+  // key не нужно типизировать
+  affair: AffairType // need to fix any
+  deleteAffairCallback: (id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
+  const deleteCallback = (id: number) => {
+    props.deleteAffairCallback(id);
+  };// need to fix
 
-    return (
-        <div>
-            // show some text
-
-            <button onClick={deleteCallback}>X</button>
-        </div>
-    )
+  return (
+    <div className={s.content}>
+      <div className={s.affair}>
+        <div
+          className={s.affairName}>{props.affair.name}</div>
+        <div
+          className={props.affair.priority === 'low' ? `${s.affairPriority} ${s.low}` : props.affair.priority === 'high' ? `${s.affairPriority} ${s.high}` : `${s.affairPriority} ${s.middle}`}>{props.affair.priority}</div>
+      </div>
+      <button className={s.button} onClick={() => deleteCallback(props.affair._id)}>X</button>
+    </div>
+  );
 }
 
-export default Affair
+export default Affair;
